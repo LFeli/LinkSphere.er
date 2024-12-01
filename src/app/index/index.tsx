925c0a652fc8f48addc9c4f87be0e8d69a1c156d
Lucas Araújo <lucasfelipemk@gmail.com>
2024-12-01 20:32:57 -0300

    :lipstick: feat: learning how to styling in react-native

---
+++ src/app/index/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./style";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World 🌎</Text>
+    </View>
+  )
+}
